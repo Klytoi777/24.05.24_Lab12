d485f26f484c0fbe7f503cad19336a809255e003
Klytoi777 <156431862+Klytoi777@users.noreply.github.com>
2024-05-24 15:52:57 +0300

    Lab12
    5+

---
+++ Pogashev/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; 
+namespace ProgramLabPogashev
+{
+    class Program{
+
+    static void Main(string[] args)
+    {
+        Console.Write("x=");
+        double x = double.Parse(Console.ReadLine());
+        double answer = Math.Sin(x)+Math.Cos(x);
+        Console.WriteLine(answer);
+        Console.ReadLine();
+    }
+
+
+    }
+}
